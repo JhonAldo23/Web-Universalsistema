bba289ebacc99f23d9958e46ff11d0aaf5fc6433
JhonAldo23 <77709502+JhonAldo23@users.noreply.github.com>
2021-01-20 12:21:29 -0500

    first commit

---
+++ form2.php
@@ -0,0 +1,12 @@
+Comentar:
+<br><br>
+Bienvenido <?php echo $_GET["username"]; ?>
+<br><br>
+¿Podríamos vivir sin el sol?: <?php echo $_GET["tipos1"]; ?>
+<br><br>
+¿Cuándo son los días más largos, en verano o en invierno?: <?php echo $_GET["tipos2"]; ?>
+<br><br>
+¿Cuánto tarda el sol en dar una vuelta alrededor del sol?: <?php echo $_GET["tipos3"]; ?>
+<br>
+<br>
+¿Cuántos planetas forman junto con el sol el sistema solar?: <?php echo $_GET["tipos4"]; ?>
